085c690b29cce99286342e12ad68f440df66b39a
James Kearney <James.Kearney@cba.com.au>
2014-05-12 13:05:06 +1000

    new homepage because hubert did not like it

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -11,9 +12,24 @@ define("port", default=8787, help="run on the given port", type=int)
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         items = { 
-                "/aji/n" : "normal addjavascriptinterface response",
-                "/aji/e" : "exploit addjavascriptinterface response"
-                }
+            "emu-writesd.html" : "write to the sd card through exploit",
+            "example1.html" : "default toast example page",
+            "id" : "shortlink id.html",
+            "id.html" : "print the id of the current user through exploit",
+            "ls" : "shortlink ls.html",
+            "ls.html" : "list the contents of /storage/sdcard0/Download",
+            "ls2" : "shortlink ls2.html",
+            "ls2.html" : "list the contents of /mnt/sdcard0/",
+            "lsd" : "shortlink lsd.html",
+            "lsd.html" : "list the contentes of /mnt/sdcard0/Download useful for after write",
+            "n" : "shortlin example1.html",
+            "old" : "redundant/working/broken attempts",
+            "run.py" : "the server",
+            "sms" : "shortlink sms.html",
+            "sms.html" : "send sms through exploit",
+            "template.html" : "server template",
+            "write" : "shortlink emu-writesd.html"
+            }
         self.render("template.html", title="AddJavascriptInterface httpd", items=items)
 
 def main():
